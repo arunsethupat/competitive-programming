5a4e47270146d77641677913bfd67684dfc6bf00
Arun Sethupat <sethupat.arun.4@gmail.com>
2018-08-10 23:31:02 -0500

    projection area 3d - Better Solution

--- project-area-3d.c
+++ project-area-3d.c
@@ -37,3 +37,20 @@ public:
         return dim1+dim2+dim3;
     }
 };
+
+
+//Better Solution
+
+int projectionArea(const vector<vector<int>>& grid) {
+        int sum = 0;
+        for (int i = 0; i < grid.size(); ++i) {
+            int m = 0, n = 0;
+            for (int j = 0; j < grid.size(); ++j) {
+                if (grid[i][j] > m) m = grid[i][j];
+                if (grid[j][i] > n) n = grid[j][i];
+                if (grid[i][j]) ++sum;
+            }
+            sum += m + n;
+        }
+        return sum;
+    }
